1e9810d647cb711cbc2850f878f2a7c4e4e3ab14
limon100 <mahmudurlimon41@gmail.com>
2020-07-29 13:12:13 +0600

    evaluate the final part

--- RF/Sonar/sonr.py
+++ RF/Sonar/sonr.py
@@ -171,15 +171,133 @@ def get_split(dataset , n_features):
     
     while(len(features) < n_features):
         
+        index = randrange(len(dataset[0]) - 1)
+        
+        if index not in features:
+            features.append(index)
+            
+        
+        for index in features:
+            features.append(index)
+        
+        return{"Index":b_index , "values":b_values , "Groups":b_groups}
+    
+        
+
+
+def to_terminal(groups):
+    
+    outcomes = [row[-1] for row in groups]
+    return max(set(outcomes) , key = outcomes.count)
 
 
 
 
+def split(node , max_depth , min_size , n_features , depth):
+    
+    left , right = node['groups']
+    del(node['groups'])
+    
+    if not left or not right:
+        node['left'] = node['right']
+        return
+    
+    if depth >= max_depth:
+        node['left'] = to_terminal(left)
+    
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+        
+
+
+
+def build_tree(train , max_depth , min_size , n_features):
+    
+    root = get_split(train , n_features)
+    split(root , max_depth , min_size , n_features , 1)
+    
+    return root
+
+
+
+def predict(node , row):
+    
+    if row[node[index]] < node['value']:
+        if isinstance(node['left'] , dict):
+            return predict(node['left'] , row)
+        
+        else:
+            return node['left']
+        
+        
+    else:
+        if isinstance(node['right'] , dict):
+            return predict(node[right] , row)
+        
+        
+
+def subsample(dataset , ratio):
+    
+    sample = list()
+    n_samples = round(len(dataset) * ratio)
+    
+    while len(sample) < n_samples:
+        index = randrange(len(dataset))
+        sample.append(dataset[index])
+        
+    return sample
+
+
+
+def bagging_predict(trees , row):
+    
+    predictions = [predict(trees , row) for tree in trees]
+    return max(set(predictions) , key = predictions.count)
 
 
 
 
+def random_forest_algorithm(train , test , max_depth , min_size , sample_size , n_trees , n_features):
+    
+    
+    trees = list()
+    
+    for i in range(n_trees):
+        
+        sample = subsample(train , sample_size)
+        tree = build_tree(sample , max_depth , min_size)
+        trees.append(tree)
+        predictions = [bagging_predict(trees , row) for row in test]
+        
+    return(predictions)
+    
 
+
+seed(2)
+
+
+
+filename = 'sonar.all-data.csv'
+dataset = load_csv(filename)
+
+
+for i in range(0 , len(dataset[0] - 1)):
+    str_to_float(dataset , i)
+    
+    
+    
+str_to_int(datset , len(dataset[0] -1))
+
+n_folds = 5
+max_depth = 10
+min_size = 1
+sample_size = 1.0
+n_features = int(sqrt(len(dataset[0] - 1)))
+
+
+for n_trees in [1 , 5 , 10]:
+    
+    scores = evaluate_algorithm(dataset , random_forest_algorithm , n_folds , max_depth , min_size , n_features)
 
 
 
